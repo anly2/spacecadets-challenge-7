1e25aad3500ce892183e4c606a3ce94932a06239
anly2 <ankos2@hotmail.com>
2013-11-21 19:21:52 +0000

    Completed Index file
    Finished the main Server file which index.php

---
+++ server/index.php
@@ -0,0 +1,195 @@
+<?
+   //Constants
+   define("default_port", 27327, 1);
+   define("setting_autoreg", true, 1);
+   define("db_users", "users.db", 1);
+   define("db_contacts", "contacts.db", 1);
+
+
+   //login
+if (isset($_REQUEST['login']))
+{
+   if (!isset($_REQUEST['user'], $_REQUEST['pass']))
+   {
+      echo "No username and password were provided!";
+      exit(1);
+   }
+
+   $users = array (
+      "anko,okna,,",
+      "halit,tilah,,"
+   );
+
+   //$users = file_exists(db_users) ? file(db_users) : array();
+   $found = false;
+
+   foreach ($users as $id => $details)
+   {
+      $details = explode(",", $details);
+
+      if ($details[0] != $_REQUEST['user'])
+         continue;
+
+      if ($details[1] != $_REQUEST['pass'])
+         continue;
+
+
+      //We got our man
+      $details[2] = $_SERVER['REMOTE_ADDR']; //His current IP
+      $details[3] = isset($_REQUEST['port']) ? $_REQUEST['port'] : default_port;
+
+      $users[$id] = implode(",", $details);
+      file_put_contents (db_users, implode("\n", $users));
+
+      $found = true;
+      echo "Sueccessfully logged in {".$details[0]."}";
+      break;
+   }
+
+   if (!$found)
+   {
+      if (!setting_autoreg)
+      {
+         echo "Username and/or password were invalid!";
+         exit (1);
+      }
+
+
+      //Automatic Registraion
+
+      $username = preg_replace ("/[^a-zA-Z0-9_\-\.@]/", "", $_REQUEST['user']);
+      $password = preg_replace ("/[^a-zA-Z0-9_ ]/", "", $_REQUEST['pass']);
+
+      $ip = $_SERVER['REMOTE_ADDR']; //His current IP
+      $port = default_port;
+
+      if (isset($_REQUEST['port']))
+         $port = preg_replace("/\D/g", "", $_REQUEST['port']);
+
+      array_push ($users, $username,$password,$ip,$port);
+      file_put_contents (db_users, implode("\n", $users));
+   }
+
+   exit (0);
+}
+
+   //query
+if (isset($_REQUEST['query']))
+{
+   if (!isset($_REQUEST['user']))
+   {
+      echo "No username provided!";
+      exit(1);
+   }
+
+   $users = array (
+      "anko,okna,,",
+      "halit,tilah,,"
+   );
+
+   //$users = file_exists(db_users) ? file(db_users) : array();
+
+   foreach ($users as $id => $details)
+   {
+      $details = explode (",", $details);
+
+      if ($details[0] != $_REQUEST['user'])
+         continue;
+
+      if ( trim($details[2].$details[3]) == "")
+      {
+         echo "Failure: User not logged in.";
+         exit (1);
+      }
+
+      echo $details[2].":".$details[3];
+      exit (0);
+   }
+
+   echo "Failure: User not found";
+   exit (1);
+}
+
+   //getContacts
+if (isset($_REQUEST['contacts']))
+{
+   if (!isset($_REQUEST['user']))
+   {
+      echo "Failure: No username provided!";
+      exit(1);
+   }
+
+
+
+   $users = array (
+      "anko,okna,,",
+      "halit,tilah,,"
+   );
+
+   $contacts = array (
+      "halit,ivo,dancho,jijo",
+      "anko,ivo,dancho,jijo"
+   );
+
+   //$users = file_exists(db_users) ? file(db_users) : array();
+   //$contacts = file_exists(db_contacts) ? file(db_contacts) : array();
+
+   foreach ($users as $id => $details)
+   {
+      $details = explode(",", $details);
+
+      if ($details[0] != $_REQUEST['user'])
+         continue;
+
+      echo $contacts[$id];
+      exit (0);
+   }
+
+   echo "Failure: User not found";
+   exit (1);
+}
+
+   //addContact
+if (isset($_REQUEST['add'], $_REQUEST['contact']))
+{
+   if (!isset($_REQUEST['user'], $_REQUEST['contact']))
+   {
+      echo "Failure: No username and contact name provided!";
+      exit(1);
+   }
+
+
+
+   $users = array (
+      "anko,okna,,",
+      "halit,tilah,,"
+   );
+
+   $contacts = array (
+      "halit,ivo,dancho,jijo",
+      "anko,ivo,dancho,jijo"
+   );
+
+   //$users = file_exists(db_users) ? file(db_users) : array();
+   //$contacts = file_exists(db_contacts) ? file(db_contacts) : array();
+
+   foreach ($users as $id => $details)
+   {
+      $details = explode(",", $details);
+
+      if ($details[0] != $_REQUEST['user'])
+         continue;
+
+      $contact = preg_replace ("/[^a-zA-Z0-9_\-\.@]/", "", $_REQUEST['user']);
+      $delimiter = (trim($contacts[$id]) == "") ? "" : ",";
+
+      $contacts[$id] .= $delimiter.$contact;
+
+      echo "Successfully added {$contact} to {".$details[0]."}'s contacts.";
+      exit (0);
+   }
+
+   echo "Failure: User not found";
+   exit (1);
+}
+?>
